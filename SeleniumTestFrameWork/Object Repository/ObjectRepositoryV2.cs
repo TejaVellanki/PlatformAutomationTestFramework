663c1ba090e8decc8faf2c63649211f2b258839f
Teja <teja.vellanki@mobankgroup.com>
2013-05-03 15:59:52 +0100

    changes to v2

--- SeleniumTestFrameWork/Object Repository/ObjectRepositoryV2.cs
+++ SeleniumTestFrameWork/Object Repository/ObjectRepositoryV2.cs
@@ -15,6 +15,13 @@ namespace ObjectRepository
     {
         public const string categorylink = "//*[@id='categoryList']/div/article";
         public const string cat = "/a/span/span[1]/h3";
+        public const string ProductPrice = "//html/body/div/div[4]/div/div/div[2]/div/div/p/strong";
+        public const string ProductDescriptiontab = "//html/body/div/div[4]/div/div[2]/div[2]/div/div/h4/a/span";
+        public const string productDescription = "//div[@id='Description']/div/div/div/p";
+        public const string detail = "css=html.ui-mobile body#page-products-details.ui-mobile-viewport div#main-page.ui-page div#mainContent.ui-content div.productDetailsContent div.ui-grid-a div.ui-block-b div.ui-collapsible-set div.ui-collapsible div.ui-collapsible-content";
+        public const string producttitle = "//html/body/div/div[4]/div/div/div/h2";
+        public const string productVariant = "Variants_0__OptionValue";
+        public const string productvariant2 = "OptionValue";
     }
 
 }
